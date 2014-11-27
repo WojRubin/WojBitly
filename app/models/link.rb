8c3f12cf7001e43c602bd3994b27bb55adf01bb9
Wojtek Maniak <wojciech@webtotal.no>
2014-11-27 07:54:02 +0100

    private link's method

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,14 @@
 class Link < ActiveRecord::Base
   attr_accessor :bitly_link
-  
+
   validates :real_link, presence: true
   after_save :create_bitly_link
 
   def self.find_real_link(bitly_link)
     self.find(Converter.new.find_id_real_link(bitly_link))
   end
+
+private
 
   def create_bitly_link
     self.bitly_link = Converter.new.to_bitly_link(self.id)
